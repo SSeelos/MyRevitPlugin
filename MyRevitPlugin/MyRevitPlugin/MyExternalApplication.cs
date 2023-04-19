93bcf0f261560273706a1fcf524374624e9a3698
SSeelos <sebastian.seelos@gmx.de>
2023-04-19 14:48:19 +0200

    init my revit plugin

---
+++ MyRevitPlugin/MyRevitPlugin/MyExternalApplication.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.UI;
+using Nice3point.Revit.Toolkit.External;
+
+namespace MyRevitPlugin
+{
+    public class MyExternalApplication : ExternalApplication
+    {
+        public override void OnStartup()
+        {
+            Application app = UiApplication.Application;
+
+            TaskDialog.Show(GetType().FullName, "Hello" + app.Username);
+        }
+    }
+}
