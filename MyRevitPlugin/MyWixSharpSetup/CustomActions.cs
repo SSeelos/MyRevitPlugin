fcc4773a40f32f1d0f079c90617bbae2c2cd74d4
SSeelos <sebastian.seelos@gmx.de>
2023-06-19 14:41:22 +0200

    custom action example

---
+++ MyRevitPlugin/MyWixSharpSetup/CustomActions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+
+namespace MyWixSharpSetup
+{
+    public static class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult MyCustomAction(Session session)
+        {
+            session.Log($"Begin {nameof(MyCustomAction)}");
+
+            return ActionResult.Success;
+        }
+    }
+}
